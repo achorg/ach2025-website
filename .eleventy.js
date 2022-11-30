ea46d78807613d3f9727c4cbdc49f19ffdd5f63c
apjanco <apjanco@gmail.com>
2022-11-30 10:09:08 -0500

    fixing migration eleventy file

--- .eleventy.js
+++ .eleventy.js
@@ -1,76 +1,16 @@
-const yaml = require("js-yaml");
-const { DateTime } = require("luxon");
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const htmlmin = require("html-minifier");
 const { EleventyI18nPlugin } = require("@11ty/eleventy");
+const yaml = require("js-yaml");
 
-module.exports = function (eleventyConfig) {
-
-  // Disable automatic use of your .gitignore
-  eleventyConfig.setUseGitIgnore(false);
-  
-  eleventyConfig.addNunjucksFilter("randomImage", arr => arr[Math.floor(Math.random()*arr.length)]);
-  
-  // Merge data instead of overriding
-  eleventyConfig.setDataDeepMerge(true);
-
-  // human readable date
-  eleventyConfig.addFilter("readableDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
-      "dd LLL yyyy"
-    );
-  });
-
-  // Syntax Highlighting for Code blocks
-  eleventyConfig.addPlugin(syntaxHighlight);
-  
- 
-  eleventyConfig.addPlugin(EleventyI18nPlugin, {
-    // any valid BCP 47-compatible language tag is supported
-    defaultLanguage: "en", // Required, this site uses "en"
-    errorMode: "never"
-  });
-
-  // To Support .yaml Extension in _data
-  // You may remove this if you can use JSON
-  eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
-
-  // Copy Static Files to /_Site
-  eleventyConfig.addPassthroughCopy({
-    "./src/admin/config.yml": "./admin/config.yml",
-    "./node_modules/alpinejs/dist/cdn.min.js": "./static/js/alpine.js",
-    "./node_modules/prismjs/themes/prism-tomorrow.css":
-    "./static/css/prism-tomorrow.css", 
-    
-  });
-
-  // Copy Image Folder to /_site
-  eleventyConfig.addPassthroughCopy("./src/static/img");
-  eleventyConfig.addPassthroughCopy("./src/static/css");
-  // Copy favicon to route of /_site
-  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
-
-  // Minify HTML
-  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
-    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
-    if (outputPath.endsWith(".html")) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true,
-      });
-      return minified;
-    }
-
-    return content;
-  });
-
-  // Let Eleventy transform HTML files as nunjucks
-  // So that we can use .html instead of .njk
-  return {
-    dir: {
-      input: "src",
-    },
-    htmlTemplateEngine: "njk",
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPlugin(EleventyI18nPlugin, {
+      // any valid BCP 47-compatible language tag is supported
+      defaultLanguage: "en", // Required, this site uses "en"
+      errorMode: "never"
+    });
+    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+    // Copy Static Files to /_Site
+    eleventyConfig.addPassthroughCopy({
+    "./admin/config.yml": "./admin/config.yml",
+    });
   };
-};
